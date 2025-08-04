8e4c587c31e3fcee65e26bb9de58709ebd9888b2
sskeysskey <728308386@qq.com>
2025-08-04 21:18:58 +0800

    Auto commit at 2025-08-04 21:18:58

--- init.lua
+++ init.lua
@@ -35,7 +35,6 @@ hs.hotkey.bind({"ctrl"}, "2", function()
   ):start()
 end)
 
-
 hs.hotkey.bind({"ctrl", "alt"}, "Z", function()
   hs.notify.new({title="Hammerspoon", informativeText="正在执行 search similar tag..."}):send()
   -- 第一个参数是可执行文件路径，第三个参数是包含脚本路径和其他参数的 table
@@ -141,6 +140,13 @@ end)
 hs.hotkey.bind({"ctrl", "alt"}, "E", function()
   hs.notify.new({title="Hammerspoon", informativeText="正在执行 Trans_small_poe.scpt 脚本..."}):send()
   local scriptPath = "/Users/yanzhang/Coding/ScriptEditor/Trans_small_poe.scpt"
+  -- 使用 hs.task 异步执行 AppleScript
+  hs.task.new("/usr/bin/osascript", nil, {scriptPath}):start()
+end)
+
+hs.hotkey.bind({"ctrl", "Shift"}, "Q", function()
+  hs.notify.new({title="Hammerspoon", informativeText="正在执行 Bob 脚本..."}):send()
+  local scriptPath = "/Users/yanzhang/Coding/ScriptEditor/Bob.scpt"
   -- 使用 hs.task 异步执行 AppleScript
   hs.task.new("/usr/bin/osascript", nil, {scriptPath}):start()
 end)
