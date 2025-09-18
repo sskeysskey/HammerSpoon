a68feb03d61efda02f3f554c174d65970e7a2958
sskeysskey <728308386@qq.com>
2025-09-18 20:33:05 +0800

    Auto commit at 2025-09-18 20:33:05

--- init.lua
+++ init.lua
@@ -113,6 +113,16 @@ hs.hotkey.bind({"ctrl"}, "'", function()
   ):start()
 end)
 
+hs.hotkey.bind({"ctrl", "alt"}, "E", function()
+  hs.notify.new({title="Hammerspoon", informativeText="正在执行 Editor_Blacklist..."}):send()
+  -- 第一个参数是可执行文件路径，第三个参数是包含脚本路径和其他参数的 table
+  hs.task.new(
+    "/Library/Frameworks/Python.framework/Versions/Current/bin/python3",
+    nil, -- 我们不需要在任务完成时执行回调函数
+    {"/Users/yanzhang/Coding/Financial_System/Operations/Editor_Blacklist.py"}
+  ):start()
+end)
+
 hs.hotkey.bind({"ctrl"}, "D", function()
   hs.notify.new({title="Hammerspoon", informativeText="正在打开财报编辑界面..."}):send()
   hs.task.new(
